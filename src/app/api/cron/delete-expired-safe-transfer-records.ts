1d915d2c5669452525980d8b82447a59645546d8
Zolastic <honhattienlul@gmail.com>
2024-08-28 20:19:52 +0800

    refactor: add authorization to cron job function

--- src/app/api/cron/delete-expired-safe-transfer-records.ts
+++ src/app/api/cron/delete-expired-safe-transfer-records.ts
@@ -1,6 +1,12 @@
 import { type NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function GET(req: NextRequest) {
+  if (
+    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
+
   console.log("Cron Job Ran at: ", new Date());
 
   return new NextResponse("Cron Ran", { status: 200 });
